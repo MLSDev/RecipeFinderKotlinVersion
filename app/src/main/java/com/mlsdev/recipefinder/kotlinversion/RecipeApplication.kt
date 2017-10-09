0505ecca9890000302fe2be028cf80bf9dea3d37
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2017-10-09 11:34:10 +0300

    Updated build gradle

--- app/src/main/java/com/mlsdev/recipefinder/kotlinversion/RecipeApplication.kt
+++ app/src/main/java/com/mlsdev/recipefinder/kotlinversion/RecipeApplication.kt
@@ -1,22 +1,11 @@
 package com.mlsdev.recipefinder.kotlinversion
 
-import android.app.Activity
 import com.mlsdev.recipefinder.kotlinversion.di.DaggerMainComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasActivityInjector
-import javax.inject.Inject
 
-class RecipeApplication : DaggerApplication(), HasActivityInjector {
+open class RecipeApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
         return DaggerMainComponent.builder().application(this).build()
-    }
-
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
-
-    override fun activityInjector(): DispatchingAndroidInjector<Activity> {
-        return dispatchingAndroidInjector;
     }
 }
